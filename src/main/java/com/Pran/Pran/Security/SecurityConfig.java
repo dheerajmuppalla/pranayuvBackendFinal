91acb7b0337010ec07424f1a67576b9ccb25b1e2
dheerajmuppalla <121094194+dheerajmuppalla@users.noreply.github.com>
2025-05-15 22:32:45 +0530

    Update SecurityConfig.java

--- src/main/java/com/Pran/Pran/Security/SecurityConfig.java
+++ src/main/java/com/Pran/Pran/Security/SecurityConfig.java
@@ -51,7 +51,7 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(Arrays.asList("https://pranayuv.com/"));
+        configuration.setAllowedOrigins(Arrays.asList("https://pranayuv.com"));
         configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(Arrays.asList("*"));
         configuration.setAllowCredentials(true);
